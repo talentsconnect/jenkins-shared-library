7224fdebafa288f70abeb3f3c7e81c003065a784
Marcel Steffen <marcel@talentsconnect.com>
2022-11-07 18:23:08 +0100

    Those stupid Strings suck

--- vars/buildAndTagContainerWithSemVer.groovy
+++ vars/buildAndTagContainerWithSemVer.groovy
@@ -6,36 +6,27 @@ def call(Map config = [:]) {
     sh "echo Repo: ${config.nexusRepository}"
     sh "echo Path: ${config.registryPath}"
 
-    sh '''
-    docker build \\
-        --build-arg BUILD_DATE=$(date -u +\'%Y-%m-%dT%H:%M:%SZ\') \\
-        --build-arg VCS_REF=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=8 HEAD) \\
-        --build-arg VERSION=${config.majorTag} \\
-        --tag ${config.nexusRepository}/${config.registryPath}:${config.majorTag} .             
-    '''
+    sh "docker build " +
+            "--build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') " +
+            "--build-arg VCS_REF=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=8 HEAD) " +
+            "--build-arg VERSION=${config.majorTag} " +
+            "--tag ${config.nexusRepository}/${config.registryPath}:${config.majorTag} ."
 
-    sh '''
-    docker build \\
-        --build-arg BUILD_DATE=$(date -u +\'%Y-%m-%dT%H:%M:%SZ\') \\
-        --build-arg VCS_REF=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=8 HEAD) \\
-        --build-arg VERSION=${config.majorMinorTag} \\
-        --tag ${config.nexusRepository}/${config.registryPath}:${config.majorMinorTag} .             
-    '''
+    sh "docker build " +
+            "--build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') " +
+            "--build-arg VCS_REF=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=8 HEAD) " +
+            "--build-arg VERSION=${config.majorMinorTag} " +
+            "--tag ${config.nexusRepository}/${config.registryPath}:${config.majorMinorTag} ."
 
-    sh '''
-    docker build \\
-        --build-arg BUILD_DATE=$(date -u +\'%Y-%m-%dT%H:%M:%SZ\') \\
-        --build-arg VCS_REF=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=8 HEAD) \\
-        --build-arg VERSION=${config.version} \\
-        --tag ${config.nexusRepository}/${config.registryPath}:${config.version} .             
-    '''
+    sh "docker build " +
+            "--build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') " +
+            "--build-arg VCS_REF=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=8 HEAD) " +
+            "--build-arg VERSION=${config.version} " +
+            "--tag ${config.nexusRepository}/${config.registryPath}:${config.version} ."
 
-    sh '''
-    docker build \\
-        --build-arg BUILD_DATE=$(date -u +\'%Y-%m-%dT%H:%M:%SZ\') \\
-        --build-arg VCS_REF=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=8 HEAD) \\
-        --build-arg VERSION=latest \\
-        --tag ${config.nexusRepository}/${config.registryPath}:latest .             
-    '''
-
+    sh "docker build " +
+            "--build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') " +
+            "--build-arg VCS_REF=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=8 HEAD) " +
+            "--build-arg VERSION=latest " +
+            "--tag ${config.nexusRepository}/${config.registryPath}:latest ."
 }
