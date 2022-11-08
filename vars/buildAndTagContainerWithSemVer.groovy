f1156a82191140123b62a4e744c507180b5ed784
Marcel Steffen <marcel@talentsconnect.com>
2022-11-08 10:40:16 +0100

    and as echo

--- vars/buildAndTagContainerWithSemVer.groovy
+++ vars/buildAndTagContainerWithSemVer.groovy
@@ -8,7 +8,7 @@ def call(Map config = [:]) {
 
     script {
         env.CURRENT_TIME = sh(returnStdout: true, script: 'date -u +%Y-%m-%dT%H:%M:%SZ').trim()
-        env.VCS_REF = sh(returnStdout: true, script: '$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=8 HEAD)').trim()
+        env.VCS_REF = sh(returnStdout: true, script: 'echo $(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=8 HEAD)').trim()
     }
 
     sh '''
