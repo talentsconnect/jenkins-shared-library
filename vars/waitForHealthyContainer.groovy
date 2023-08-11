19e4c1baa37010cb0a5f717f5ea5210f898c7829
Marcel Steffen <marcel@talentsconnect.com>
2023-08-11 20:01:32 +0200

    AS-188 experiment with wait script

--- vars/waitForHealthyContainer.groovy
+++ vars/waitForHealthyContainer.groovy
@@ -2,6 +2,7 @@ def call(Map config = [:]) {
 
     script {
         sh '''
+            echo here
             REXP=jobshop_test_stack_${config.refId}_${config.serviceName}
             containerId = docker ps | awk "/$REF/"'{print $1}'
             containerState = docker inspect -f {{.State.Health.Status}} $containerId
